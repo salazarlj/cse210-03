dcb97f085e64745191798075bd7c768d9cc57c60
melaniecristeche <u943142818@byui.edu>
2022-02-05 20:07:51 -0300

    commit

--- Jumper_Game/Classes/Display.py
+++ Jumper_Game/Classes/Display.py
@@ -6,3 +6,8 @@ class Display:
     def __init__(self) -> None:
         
         pass
+
+    def draw_lines():
+        pass
+    def draw_parachute():
+        pass
--- Jumper_Game/Classes/Guess.py
+++ Jumper_Game/Classes/Guess.py
@@ -7,5 +7,10 @@ class Guess:
     
     def __init__(self):
         secret_word=Puzzle.get_random_word(Puzzle())
+    
+    def guess_letter():
+        pass
+    def guess_word():
+        pass
 
 
