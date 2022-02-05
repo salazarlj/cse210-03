55b70aa2f2ec1842a2ac35f3068305adb3d7d2f1
carteraymond <ray21006@byui.edu>
2022-02-05 14:45:59 -0500

    more work done with puzzle and Guess

--- Jumper_Game/Classes/Puzzle.py
+++ Jumper_Game/Classes/Puzzle.py
@@ -1,9 +1,18 @@
 
-
-
+import random
 
 class Puzzle:
     
     def __init__(self) -> None:
+        #needs lots of words in the list
+        self.word_list=["dog","tree","horse","coding","gato","hands","elephant","cucumber","pickle","mouse","moose","toenail","blanket","pillow","shirt","glass","door",]
         
-        pass
+        
+        
+    def get_random_word(self):
+        secret_word=random.sample(self.word_list,1)
+    
+        return secret_word
+    
+# puzzle=Puzzle()
+# Puzzle.get_random_word(puzzle)
