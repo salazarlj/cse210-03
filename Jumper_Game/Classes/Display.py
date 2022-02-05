4afb04494a761bc009b707ac4e2064e761787afa
melaniecristeche <u943142818@byui.edu>
2022-02-05 20:33:27 -0300

    commit for little changes

--- Jumper_Game/Classes/Display.py
+++ Jumper_Game/Classes/Display.py
@@ -2,12 +2,27 @@
 
 
 class Display:
+
+    """ This class is in charge of displaying the lines and
+     organizing the parachute structure
+     
+    attributes:
+    
+    methods:"""
     
     def __init__(self) -> None:
-        
+        print("_ _ _ _ _ ")
         pass
 
     def draw_lines():
         pass
     def draw_parachute():
+        print('  +---------+')    
+        print('  |         |')    
+        print('  X         |')    
+        print(' /|\        |')    
+        print(' / \        |')    
+        print('            |')    
+        print('            |')    
+        print('  ---------------------')
         pass
