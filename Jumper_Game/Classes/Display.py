0931eb35a2cb71e4ce0e8e5bea19ebdc76004dd3
Leonardo Salazar <salazarlj@gmail.com>
2022-02-05 20:06:02 -0600

    This commit created Display Class to draw parachute

--- Jumper_Game/Classes/Display.py
+++ Jumper_Game/Classes/Display.py
@@ -3,6 +3,22 @@
 
 class Display:
     
-    def __init__(self) -> None:
-        
-        pass
+    def __init__(self):
+        self.space = " "
+       
+    def draw_person(self):
+        print("\t"+self.space+"O")
+        print("\t/|\\")
+        print("\t/ \\")
+      
+    def draw_parachute(self):
+        print ("\t"+self.space+"__")
+        print("\t/__\\")
+        print("\t\  /")
+        print("\t \/")
+   
+test1 = Display()
+
+test1.draw_parachute()
+
+test1.draw_person()
