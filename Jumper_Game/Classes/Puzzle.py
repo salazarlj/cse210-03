deb647d22517f2dfdb2f2b0a18ea6e516cf50517
VicGa <90812185+VicGa@users.noreply.github.com>
2022-02-07 23:41:59 -0300

    rewrite the code focus only in guess class

--- Jumper_Game/Classes/Puzzle.py
+++ Jumper_Game/Classes/Puzzle.py
@@ -14,8 +14,7 @@ class Puzzle:
     def __init__(self) -> None:
         #needs lots of words in the list
         self.word_list=["dog","tree","horse","coding","gato","hands","elephant","cucumber","pickle","mouse","moose","toenail","blanket","pillow","shirt","glass","door",]
-        self.word_current = ''
-        self.letters_word = []
+       
         
         
     def get_random_word(self):
@@ -23,12 +22,7 @@ class Puzzle:
         # Generate a random word
 
         secret_word=random.sample(self.word_list,1)
-        word_list2 = []
-        self.word_current = self.word_list2[secret_word -1]
-        #Create a list with the letter from the secret word
-        word = self.word_current
-        for i in range (len(word)):
-            self.letters_word.append(word[i])
+        
     
         return secret_word
     
