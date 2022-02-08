6c9aa9b46ae3a12de54cb97bffb6035d888c0938
VicGa <90812185+VicGa@users.noreply.github.com>
2022-02-07 22:02:04 -0300

    add by victor daniel gauna

--- Jumper_Game/Classes/Guess.py
+++ Jumper_Game/Classes/Guess.py
@@ -1,22 +1,51 @@
+from io import StringIO
 from Puzzle import Puzzle
 
 
 class Guess:
-    
+
     """This class is in charge of the player´s guessing letter part
 
     attributes:
-    
+
     methods:
 
     """
-    
+
     def __init__(self):
-        secret_word=Puzzle.get_random_word(Puzzle())
-    
-    def guess_letter():
+        secret_word = Puzzle.get_random_word(Puzzle())
+        self.letters_word = self.Puzzle.letters_word
+        self.letters_hidden = []
+
+    def guess_letter(self):
+        ask = input('Guess a letter [a-z]: ')
+        ask = ask.strip().lower
+
+        valid_word = self.Puzzle.check_letters(ask)
+        if valid_word:
+            self.do_inputs = ask[0]
+        else:
+            print("That's not a valid character...")
         pass
+    def list_dashes(self):
+        for i in range(len(self.letters_word)):
+            self.letters_hidden.append('_')
+
+    def check_letters(self, character) -> bool:
+        stringg = StringIO.ascii_lowercase
+        listt = list(stringg)
+        if character in listt:
+            return True
+        else:
+            return False
+    def check_guessed(self):
+        # Chek if the letter choosed by the user is in the secret word, and remplace the dashes if there is the letter. 
+         if self.do_inputs in self.letters_word:
+             for i in range (len(self.letters_word)):
+                 if self.do_inputs == self.letters_word[i]:
+                     self.letters_hidden[i] = self.do_inputs
+                     pass
+
+
     def reveal_word():
         pass
-
-
--- Jumper_Game/Classes/Puzzle.py
+++ Jumper_Game/Classes/Puzzle.py
@@ -14,11 +14,21 @@ class Puzzle:
     def __init__(self) -> None:
         #needs lots of words in the list
         self.word_list=["dog","tree","horse","coding","gato","hands","elephant","cucumber","pickle","mouse","moose","toenail","blanket","pillow","shirt","glass","door",]
-        
+        self.word_current = ''
+        self.letters_word = []
         
         
     def get_random_word(self):
+
+        # Generate a random word
+
         secret_word=random.sample(self.word_list,1)
+        word_list2 = []
+        self.word_current = self.word_list2[secret_word -1]
+        #Create a list with the letter from the secret word
+        word = self.word_current
+        for i in range (len(word)):
+            self.letters_word.append(word[i])
     
         return secret_word
     
