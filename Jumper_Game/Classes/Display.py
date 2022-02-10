37fa3270bc654580ef8b4b0876bf6e1f355e9b41
carteraymond <ray21006@byui.edu>
2022-02-09 19:00:32 -0500

    fixed display

--- Jumper_Game/Classes/Display.py
+++ Jumper_Game/Classes/Display.py
@@ -9,18 +9,41 @@ class Display:
     methods: """
     
     def __init__(self):
-        self.space = " "
+        self.chute = ["\t __","\t/__\\","\t\  /","\t \/"]
+        self.life=4
        
     def draw_person(self):
-        print("\t"+self.space+"O")
-        print("\t/|\\")
-        print("\t/ \\")
+        if self.life>0:
+            print( "\t O")
+        else:
+            print( "\t X")
+        print(" \t/|\\")
+        print(" \t/ \\")
       
     def draw_parachute(self):
-        print ("\t"+self.space+"__")
-        print("\t/__\\")
-        print("\t\  /")
-        print("\t \/")
+        if self.life==4:
+            for j in self.chute:
+                print(j)
+        elif self.life==3:
+            for i in range(1):
+                self.chute.pop(0)
+            for i in self.chute:
+                print(i)
+        elif self.life==2:
+            for i in range(2):
+                self.chute.pop(0)
+            for i in self.chute:
+                print(i)
+        elif self.life==1:
+            for i in range(3):
+                self.chute.pop(0)
+            for i in self.chute:
+                print(i)
+        else:
+            pass
+                
+                
+
    
 test1 = Display()
 
